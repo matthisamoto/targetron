cd110ae08630d237340faee5681eef1c919604e8
Matt Hisamoto <mwhisamoto@gmail.com>
2012-07-12 16:31:06 -0400

    js clean

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,28 +3,21 @@
 //= require jquery.address-1.4.min
 
 var Targetron = {
-	
 	persona:    0,
 	descriptor: 0,
 	action:     0,
-	
 	address: function() {
-		console.log("address fire")
 		persona =    parseInt($('input#persona').val());
 		descriptor = parseInt($('input#descriptor').val());
 		action =     parseInt($('input#action').val());
-		
 		$.address.value('/');
 		$.address.parameter('p', Targetron.persona,    true);
 		$.address.parameter('d', Targetron.descriptor, true);
 		$.address.parameter('a', Targetron.action,     true);
 		$.address.update();
 	}
-	
 }
 
 $(function() {
-	
 	Targetron.address();
-
 });
