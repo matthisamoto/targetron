d84298d5fda7e95cb509a2238ad1b216cc1bbd3d
Matt Hisamoto <mwhisamoto@gmail.com>
2012-07-13 10:17:35 -0400

    removed old vendor, url updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,20 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery.address-1.4.min
 
 var Targetron = {
 	persona:    0,
 	descriptor: 0,
 	action:     0,
 	address: function() {
-		persona =    parseInt($('input#persona').val());
-		descriptor = parseInt($('input#descriptor').val());
-		action =     parseInt($('input#action').val());
-		$.address.value('/');
-		$.address.parameter('p', Targetron.persona,    true);
-		$.address.parameter('d', Targetron.descriptor, true);
-		$.address.parameter('a', Targetron.action,     true);
-		$.address.update();
+		Targetron.persona =    parseInt($('input#persona').val());
+		Targetron.descriptor = parseInt($('input#descriptor').val());
+		Targetron.action =     parseInt($('input#action').val());
+		
+		
+		var state = "?p=" + Targetron.persona + "&d=" + Targetron.descriptor + "&a=" + Targetron.action; // { "p": Targetron.persona, "d": Targetron.descriptor, "a": Targetron.action }
+		
+		window.history.replaceState({}, document.title, state);
+		
 	}
 }
 
